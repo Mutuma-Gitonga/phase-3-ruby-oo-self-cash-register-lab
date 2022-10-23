0664e47bdb1de8078245b6a2f545a3bf28c202b4
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-23 15:38:12 +0300

    Completed assignment - New Attempt

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,55 +2,26 @@ require 'pry'
 
 class CashRegister
   
-  attr_accessor :total, :discount
-
-  @@added_items = []
-  @@new_object_created = false
-  @@transactions_tracker = []
+  attr_accessor :total, :discount, :items, :transactions
 
   def initialize(discount = nil)
-    @@new_object_created = true
     @discount = discount
     @total = 0
+    @items = []
+    @transactions = []
   end
 
 
   def add_item (title, price, quantity = 1)
 
-   
-
-
-    qty = quantity
-
-    if(@@new_object_created)
-
-      if(@@added_items.empty?())
-        while qty > 0
-          @@transactions_tracker << [price, quantity]
-          @@added_items << title
-          qty -= 1
-        end
-        @@new_object_created = false
-      else
-        @@transactions_tracker.clear
-        @@added_items.clear
-        while qty > 0
-          @@transactions_tracker << [price, quantity]
-          @@added_items << title
-          qty -= 1
-        end
-        @@new_object_created = false
-      end
-    else
-
-      while qty > 0
-        @@transactions_tracker << [price, quantity]
-        @@added_items << title
-        qty -= 1
-      end
-    end
-
     self.total += price * quantity
+
+    x = 0 
+    until x == quantity do
+      self.items << title
+      self.transactions << [price, quantity]
+      x += 1
+    end
     
   end
    
@@ -66,34 +37,11 @@ class CashRegister
 
 
   def items
-    @@added_items
+    @items
   end
 
   def void_last_transaction
-
-    self.total -= @@transactions_tracker.last()[0] * @@transactions_tracker.last()[1]
+    self.total -= @transactions.last()[0] * @transactions.last()[1]
   end
   
 end 
-
-# cash_register = CashRegister.new
-# cash_register.add_item("apple", 0.99)
-# cash_register.add_item("tomato", 1.76)
-# binding.pry
-
-# another_register = CashRegister.new
-# another_register.add_item("cheese", 2)
-# another_register.add_item("rice", 2)
-# # binding.pry
-
-# new_register = CashRegister.new
-# new_register.add_item("eggs", 1.99, 2)
-# new_register.add_item("tomato", 1.76, 3)
-# new_register.add_item("cherry", 1.55, 2)
-# binding.pry
-
-# third_register = CashRegister.new
-# third_register.add_item("Ugali",3)
-# third_register.add_item("nduma",3)
-# binding.pry
-
